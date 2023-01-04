cedd16414fc595a3794d40bf4e5e663b8e31f416
DESKTOP-PCO45IQ\DAL <3452720699@qq.com>
2023-01-04 15:58:37 +0800

    3c+目标 （胜利或失败）。设置游戏通关条件，协同程序实现延迟调用

---
+++ Assets/Scripts/LevelPass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelPass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject levelPassText;
+    void Start()
+    {
+        if (levelPassText == null)
+            levelPassText = GameObject.Find("Canvas").transform.Find("levelPassText").gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        levelPassText.SetActive(true);
+        StartCoroutine(Dely(2, () =>
+        {
+            SceneManager.LoadScene(currentScene.name);
+        }));
+    }
+    IEnumerator Dely(float seconds, Action onFinish)
+    {
+        yield return new WaitForSeconds(seconds);
+        onFinish?.Invoke();
+    }
+}
