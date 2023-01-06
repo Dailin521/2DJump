c0527f18a623aa609a660674d32afe3b97c712d8
DESKTOP-PCO45IQ\DAL <3452720699@qq.com>
2023-01-06 17:04:43 +0800

    镜头跟随+地块绘制

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, Mathf.Clamp((player.position.y + 2), 2, 8), -10);
+    }
+}
